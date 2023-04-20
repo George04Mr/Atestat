e9db48566d02f6d548588690d2dac1a4ce1e7f79
George04Mr <georgedregan.27@gmail.com>
2023-04-20 18:21:28 +0300

    first commit

---
+++ app/src/main/java/com/georgedregan/atestatapp/ui/SignInActivity.kt
@@ -0,0 +1,12 @@
+package com.georgedregan.atestatapp.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.georgedregan.atestatapp.R
+
+class SignInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signin)
+    }
+}
