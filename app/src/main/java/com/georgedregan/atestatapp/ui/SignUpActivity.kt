be461b8fece2b01f23898574ea85893e80663c27
Cristian Dregan <cristi.dregan@tapptitude.com>
2023-04-23 14:54:02 +0300

    Changed App Name and Icon

--- app/src/main/java/com/georgedregan/atestatapp/ui/SignUpActivity.kt
+++ app/src/main/java/com/georgedregan/atestatapp/ui/SignUpActivity.kt
@@ -1,12 +1,35 @@
 package com.georgedregan.atestatapp.ui
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.view.ViewTreeObserver
 import androidx.appcompat.app.AppCompatActivity
 import com.georgedregan.atestatapp.R
 
 class SignUpActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+
+        val content: View = findViewById(android.R.id.content)
+        content.viewTreeObserver.addOnPreDrawListener(
+            object : ViewTreeObserver.OnPreDrawListener {
+                override fun onPreDraw(): Boolean {
+
+                    // Check if user is logged in
+                    // ToDo Check from DB
+                    return if (true) {
+                        // The content is ready. Start drawing.
+                        content.viewTreeObserver.removeOnPreDrawListener(this)
+                        true
+                    } else {
+                        startActivity(Intent(this@SignUpActivity, MainActivity::class.java))
+                        false
+                    }
+                }
+            }
+        )
     }
 }
